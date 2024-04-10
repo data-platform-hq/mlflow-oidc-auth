f40f10de3950b9115ac0d74e48d95e3c031a411b
Alexander Kharkevich <alexander_kharkevich@outlook.com>
2024-04-09 22:40:28 -0400

    feat: ui integration (#10)
    Co-authored-by: Aleksei Moiseev <aliaksei_maiseyeu@epam.com>

--- web-ui/src/app/features/admin-page/components/details/user-permission-details/user-permission-details.config.ts
+++ web-ui/src/app/features/admin-page/components/details/user-permission-details/user-permission-details.config.ts
@@ -1,3 +1,5 @@
+import { TABLE_ACTION_CONFIG } from 'src/app/shared/components/table/table.config';
+
 export const MODEL_COLUMN_CONFIG = [
   { title: 'Model name', key: 'name' },
   { title: 'Permissions', key: 'permissions' },
@@ -6,4 +8,14 @@ export const MODEL_COLUMN_CONFIG = [
 export const EXPERIMENT_COLUMN_CONFIG = [
   { title: 'Experiment Name', key: 'name' },
   { title: 'Permission', key: 'permissions' },
+];
+
+export const EXPERIMENT_ACTIONS = [
+  TABLE_ACTION_CONFIG.EDIT,
+  TABLE_ACTION_CONFIG.REVOKE,
+];
+
+export const MODEL_ACTIONS = [
+  TABLE_ACTION_CONFIG.EDIT,
+  TABLE_ACTION_CONFIG.REVOKE,
 ];
