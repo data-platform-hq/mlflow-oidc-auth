396d7f5c98577a479aa4293413304cce7e478919
mshodge <michael.hodge@digital.justice.gov.uk>
2024-12-13 08:54:16 +0000

    adds admin flag to user name in UI

--- web-ui/src/app/shared/components/header/header.component.ts
+++ web-ui/src/app/shared/components/header/header.component.ts
@@ -7,6 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   @Input() name: string = '';
+  @Input() admin: boolean = false;
 
   constructor() { }
 
