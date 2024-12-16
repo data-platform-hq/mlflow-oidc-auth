ab457c3d0be67ce6fd59f443f519b62a2586dcce
Alexander Kharkevich <alexander_kharkevich@epam.com>
2024-12-16 12:32:34 -0500

    chore: move get permission to utils

--- mlflow_oidc_auth/validators/registered_model.py
+++ mlflow_oidc_auth/validators/registered_model.py
@@ -1,8 +1,6 @@
 from mlflow_oidc_auth.permissions import Permission
 from mlflow_oidc_auth.store import store
-from mlflow_oidc_auth.utils import get_request_param, get_username
-
-from ._permissions import get_permission_from_store_or_default
+from mlflow_oidc_auth.utils import get_permission_from_store_or_default, get_request_param, get_username
 
 
 def _get_permission_from_registered_model_name() -> Permission:
