f840eac0d25db6d7b30bd7f99bec46181c987881
Ion Koutsouris <15728914+ion-elgreco@users.noreply.github.com>
2024-12-17 06:37:55 +0100

    fix: ui components missing in release (#61)

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 export MLFLOW_OIDC_AUTH_VERSION=${1:-0.0.0.dev0}
-pushd web-ui
-yarn install
-yarn release
-popd
+pushd web-ui &&\
+yarn install &&\
+yarn release &&\
+popd &&\
 python -m build
